f53fb40af609340e3b9fdccccc0eefc8817ed054
Rani <raniachheylal24@navgurukul.org>
2026-05-04 11:32:37 +0530

    Update home page content

--- routes/home.js
+++ routes/home.js
@@ -7,6 +7,8 @@ router.get("/", isLoggedIn, homeController.index);
 router.get("/about", homeController.about);
 router.get("/products", homeController.products);
 router.get("/blogs", homeController.blogs);
+router.get("/blogs/:idOrSlug", homeController.articleDetails);
+router.get("/article/:idOrSlug", homeController.articleDetails);
 router.get("/privacy-policy", homeController.privacy);
 router.get("/terms", homeController.terms);
 router.get("/contact", homeController.contact);
